0f347479a21b3a992de35d00f4b82e26d63c80ce
Minim131 <100144145+Minim131@users.noreply.github.com>
2022-07-26 11:16:20 +0800

    Added the year in

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function FormattedDate(props) {
+  console.log(props);
   let days = [
     "Sunday",
     "Monday",
@@ -27,12 +28,13 @@ export default function FormattedDate(props) {
   let day = days[props.date.getDay()];
   let date = props.date.getDate();
   let month = months[props.date.getMonth()];
+  let year = props.date.getFullYear();
   if (date < 10) {
     date = `0${date}`;
   }
   return (
     <div>
-      {day}, {date} {month}
+      {day}, {date} {month} {year}
     </div>
   );
 }
